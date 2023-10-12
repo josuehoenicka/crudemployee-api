71042a1702a686c45ce121f5139ac4e4835746f9
josuehoenicka <josuee.vzla@gmail.com>
2023-10-12 18:25:12 -0300

    bug: connection to sql server

---
+++ CrudEmployee.API/Data/CrudEmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿// Microsoft Core
+using Microsoft.EntityFrameworkCore;
+
+// Models
+using CrudEmployee.API.Models;
+
+namespace CrudEmployee.API.Data
+{
+    public class CrudEmployeeDbContext : DbContext
+    {
+        public CrudEmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CrudEmployee.API/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudEmployee.API.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+
+    }
+}
